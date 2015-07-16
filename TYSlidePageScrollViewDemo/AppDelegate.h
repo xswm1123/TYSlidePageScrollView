14b3edc0caf2a6f9b4dacf02005678114d5dc95f
sunyong0329 <233wangxiao@233wangxiaotekiiMac.local>
2015-07-16 08:36:48 +0800

    Initial Commit

---
+++ TYSlidePageScrollViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TYSlidePageScrollViewDemo
+//
+//  Created by SunYong on 15/7/16.
+//  Copyright (c) 2015年 tanyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TYSlidePageScrollViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TYSlidePageScrollViewDemo
+//
+//  Created by SunYong on 15/7/16.
+//  Copyright (c) 2015年 tanyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
