147d7d08036914f4b01d41d1f7f947c717c81638
sunyong0329 <233wangxiao@233wangxiaotekiiMac.local>
2015-07-17 09:36:25 +0800

    add controller

---
+++ TYSlidePageScrollViewDemo/TYSlidePageScrollView/TYSlidePageScrollViewController.m
@@ -0,0 +1,76 @@
+//
+//  TYSlidePageScrollViewController.m
+//  TYSlidePageScrollViewDemo
+//
+//  Created by SunYong on 15/7/17.
+//  Copyright (c) 2015年 tanyang. All rights reserved.
+//
+
+#import "TYSlidePageScrollViewController.h"
+
+@interface TYSlidePageScrollViewController ()
+@property (nonatomic, weak) TYSlidePageScrollView *slidePageScrollView;
+@end
+
+@implementation TYSlidePageScrollViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    [self addSlidePageScrollView];
+    
+    [self addViewControllers];
+}
+
+- (void)addSlidePageScrollView
+{
+    TYSlidePageScrollView *slidePageScrollView = [[TYSlidePageScrollView alloc]initWithFrame:self.view.bounds];
+    slidePageScrollView.dataSource = self;
+    slidePageScrollView.delegate = self;
+    [self.view addSubview:slidePageScrollView];
+    _slidePageScrollView = slidePageScrollView;
+}
+
+- (void)addViewControllers
+{
+    for (UIViewController *viewVC in _viewControllers) {
+        [self addChildViewController:viewVC];
+    }
+}
+
+#pragma mark - TYSlidePageScrollViewDataSource
+
+// if you want to deal with dataSource , you can override
+- (NSInteger)numberOfPageViewOnSlidePageScrollView
+{
+    return _viewControllers.count;
+}
+
+- (UIScrollView *)slidePageScrollView:(TYSlidePageScrollView *)slidePageScrollView pageVerticalScrollViewForIndex:(NSInteger)index
+{
+    UIViewController<UIViewControllerDisplayViewDelegate> *viewController = _viewControllers[index];
+    if ([viewController respondsToSelector:@selector(displayView)]) {
+        return [viewController displayView];
+    }else if ([viewController.view isKindOfClass:[UIScrollView class]]) {
+        return (UIScrollView *)viewController.view;
+    }
+    return nil;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
