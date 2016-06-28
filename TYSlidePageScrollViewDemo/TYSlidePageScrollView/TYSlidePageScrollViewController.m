583fd52d445afd868acfc9eed41626a354e708c9
tany <122074809@qq.com>
2016-06-28 17:12:00 +0800

    Update TYSlidePageScrollViewController.m

--- TYSlidePageScrollViewDemo/TYSlidePageScrollView/TYSlidePageScrollViewController.m
+++ TYSlidePageScrollViewDemo/TYSlidePageScrollView/TYSlidePageScrollViewController.m
@@ -59,7 +59,7 @@
         [self addChildViewController:viewController];
     }
     
-    if ([viewController conformsToProtocol:@protocol(UIViewControllerDisplayViewDelegate)]) {
+    if ([viewController respondsToSelector:@selector(displayView)]) {
         return [viewController displayView];
     }else if([viewController isKindOfClass:[UITableViewController class]]){
         return ((UITableViewController *)viewController).tableView;
